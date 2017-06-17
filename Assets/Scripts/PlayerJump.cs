605fe28a1c2ce7ee08db76c2a96ded0daa8e3ac0
Ju Sung Park <pkjs91@naver.com>
2017-06-18 01:44:24 +0900

    06.18 1:44am

--- Assets/Scripts/PlayerJump.cs
+++ Assets/Scripts/PlayerJump.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerJump : MonoBehaviour {
     Animator anim;
 
+    public float rotationLimit;
+
 	void Start () {
         anim = GetComponent<Animator>();
 	}
@@ -14,6 +16,13 @@ public class PlayerJump : MonoBehaviour {
         if (Input.GetButtonDown("Jump"))
         {
             anim.SetTrigger("Jump");
+        }
+
+        // 애니메이션 후 캐릭터가 회전하길래 임시적으로 막음
+        if (!(Mathf.Abs(transform.eulerAngles.y) < rotationLimit || Mathf.Abs(transform.eulerAngles.y) > 360 - rotationLimit))
+        {
+            transform.rotation = Quaternion.Euler(Vector3.zero);
+            Debug.Log("회전 재조정");
         }
 	}
 }
